3522b58f4836bef2b0334ba7cba99ff717f278d5
Darren Guo <richmondhilldarreng@gmail.com>
2021-01-16 19:49:22 -0500

    Updates

--- soundPy.py
+++ soundPy.py
@@ -1,12 +1,13 @@
 from playsound import playsound
 from configparser import ConfigParser
 import time
+import json
 
-parser = ConfigParser()
-parser.read('./config/config.ini')
+f = open('./config/config.json', 'r')
+data = json.load(f)
 
 while True:
     time.sleep(5.8)
-    playsound(parser.get("settings", "soundfile"))
+    playsound(data["SoundFile"])
     
 
