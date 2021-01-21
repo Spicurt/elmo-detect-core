4532c91d3c8c54f613ae7dee19672eaa6bb132a0
Spicurt <richmondhilldarreng@gmail.com>
2021-01-21 13:14:11 -0500

    added eye detection for more acuracy

--- pictureTake.py
+++ pictureTake.py
@@ -16,39 +16,13 @@ Sounds = [
 from SoundConfig import SoundStart
 from SoundConfig import helloElmo
 
-def highlightFace(net, video, conf_threshold=0.7):
-    videoOpencvDnn=video.copy()
-    videoHeight=videoOpencvDnn.shape[0]
-    videoWidth=videoOpencvDnn.shape[1]
-    blob=cv2.dnn.blobFromImage(videoOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
-    net.setInput(blob)
-    detections=net.forward()
-    faceBoxes=[]
-    for i in range(detections.shape[2]):
-        confidence=detections[0,0,i,2]
-        if confidence>conf_threshold:
-            x1=int(detections[0,0,i,3]*videoWidth)
-            y1=int(detections[0,0,i,4]*videoHeight)
-            x2=int(detections[0,0,i,5]*videoWidth)
-            y2=int(detections[0,0,i,6]*videoHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            cv2.rectangle(videoOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(videoHeight/150)), 8)
-    return videoOpencvDnn,faceBoxes
-
-
-parser=argparse.ArgumentParser()
-parser.add_argument('--image')
-
-args=parser.parse_args()
-
-faceProto="./Resource/opencv_face_detector.pbtxt"
-faceModel="./Resource/opencv_face_detector_uint8.pb"
-ageProto="./Resource/age_deploy.prototxt"
-ageModel="./Resource/age_net.caffemodel"
-genderProto="./Resource/gender_deploy.prototxt"
-genderModel="./Resource/gender_net.caffemodel"
-
+faceProto = "./Resource/opencv_face_detector.pbtxt"
+faceModel = "./Resource/opencv_face_detector_uint8.pb"
+ageProto = "./Resource/age_deploy.prototxt"
+ageModel = "./Resource/age_net.caffemodel"
+genderProto = "./Resource/gender_deploy.prototxt"
+genderModel = "./Resource/gender_net.caffemodel"
+eyeCascade = cv2.CascadeClassifier("./Resource/haarcascade_eye.xml")
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList=['Male','Female']
@@ -57,8 +31,7 @@ faceNet=cv2.dnn.readNet(faceModel,faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
-def detection(self):
-    import detect
+
 img = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -76,16 +49,25 @@ hiElmo.start()
 #PICTURE TAKE START
 while True:
     ret, frame = img.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.putText(frame,'Press space to take start.',(15, 15),font, 0.5,(0, 0, 0),2,cv2.LINE_4) 
     cv2.putText(frame,'Press escape to leave.',(15, 30),font, 0.5,(0, 0, 0),2,cv2.LINE_4)
+    cv2.putText(frame,'Press any other key to reload camera.',(15, 45),font, 0.5,(0, 0, 0),2,cv2.LINE_4) 
+    eye = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in eye:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     if not ret:
         print("failed to grab frame")
         break
     cv2.imshow("Picture Take", frame)
-
-    
 
     k = cv2.waitKey(1)
     if k%256 == 27:
@@ -108,8 +90,27 @@ while True:
         video=cv2.imread('./images/Photo.png')
         padding=20
         while True:
+            def highlightFace(net, video, conf_threshold=0.7):
+                videoOpencvDnn=video.copy()
+                videoHeight=videoOpencvDnn.shape[0]
+                videoWidth=videoOpencvDnn.shape[1]
+                blob=cv2.dnn.blobFromImage(videoOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+                net.setInput(blob)
+                detections=net.forward()
+                faceBoxes=[]
+                for i in range(detections.shape[2]):
+                    confidence=detections[0,0,i,2]
+                    if confidence>conf_threshold:
+                        x1=int(detections[0,0,i,3]*videoWidth)
+                        y1=int(detections[0,0,i,4]*videoHeight)
+                        x2=int(detections[0,0,i,5]*videoWidth)
+                        y2=int(detections[0,0,i,6]*videoHeight)
+                        faceBoxes.append([x1,y1,x2,y2])
+                        cv2.rectangle(videoOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(videoHeight/150)), 8)
                 
-            resultImg,faceBoxes=highlightFace(faceNet,video)
+                return videoOpencvDnn,faceBoxes
+            
+            video,faceBoxes=highlightFace(faceNet,video)
             if not faceBoxes:
                print("No face detected")
 
@@ -117,7 +118,7 @@ while True:
                 face=video[max(0,faceBox[1]-padding):
                     min(faceBox[3]+padding,video.shape[0]-1),max(0,faceBox[0]-padding)
                     :min(faceBox[2]+padding, video.shape[1]-1)]
-
+                
             blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
             genderNet.setInput(blob)
             genderPreds=genderNet.forward()
@@ -125,8 +126,9 @@ while True:
             ageNet.setInput(blob)
             agePreds=ageNet.forward()
             age=ageList[agePreds[0].argmax()]
-            cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (100,0,0), 2, cv2.LINE_AA)
-            cv2.imshow("Detecting age and gender", resultImg)
+            cv2.putText(video, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,0), 2, cv2.LINE_AA)
+            cv2.imshow("Detecting age and gender", video)
                     
             if cv2.waitKey(0):
                 break
+            
